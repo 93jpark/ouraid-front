ddade4671100b40ec05f758633bb133c22828846
Joonwoo Park <93jpark@gmail.com>
2022-07-03 12:50:57 +0900

    implements React App Router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,13 +3,15 @@ import './index.css';
 import App from './App';
 import Login from './Login';
 import SignUp from './SignUp';
+import Footer from './Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import { Typography, Link } from '@mui/material';
+import { Typography, Link, Container } from '@mui/material';
 
 const Copyright = (props) => {
     return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      
+      <Typography variant="body2" color="text.secondary" align="center" {...props} sx={{ position:"relative",  }}>
         {'Copyright © '}
         <Link color="inherit" href="https://mui.com/">
           Your Website
@@ -22,18 +24,23 @@ const Copyright = (props) => {
 
 const AppRouter = () => {
     return (
-        <div>
+        <>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/" element={<App/>} />
-                    <Route path="/signup" element={<SignUp/>}/>
+                    <Route path="/signup" element={<SignUp />}/>
                 </Routes>
-                <Box mt={5}>
-                    <Copyright/>
-                </Box>
+              
+                <Container component="footer" maxWidth="md" sx={{ 
+                                  borderTop: (theme) => `1px sold ${theme.palette.divider}`,
+                                  mt: 2,
+                                  py: [3, 6],
+                                  }} >
+                    <Footer/>
+                </Container>
             </BrowserRouter>
-        </div>
+        </>
     )
 }
 
