50da75b66f490b7db41caf61b953af783f324447
Joonwoo Park <93jpark@gmail.com>
2022-07-03 12:50:57 +0900

    add dnf main & sub class map

---
+++ src/classes.js
@@ -0,0 +1,40 @@
+export default {
+    "mainClass" : [
+        { name: "남귀검", value: "male_ghost_knight" }, 
+        { name: "여귀검", value: "female_ghost_knight" },
+        { name: "여격투가", value: "female_fighter" },
+        { name: "남거너", value: "male_gunner" },
+        { name: "여법사", value: "female_mage" },
+        { name: "여프리", value: "female_priest" },
+    ],
+    "female_ghost_knight" : [
+        { name: "소드마스터", value : "sword_master" },
+        { name: "다크템플러", value : "dark_templar" },   
+        { name: "데몬슬레이어", value : "demon_slayer" },
+        { name: "베가본드", value : "vagabond" }
+    ],
+    "male_ghost_knight" : [
+        { name: "소울브링어", value : "soul_bringer" },   
+        { name: "버서커", value : "berserker" },
+        { name: "웨펀마스터", value : "weapon_master" },
+        { name: "아수라", value : "asura" }
+    ],
+    "female_fighter" : [
+        { name: "넨마스터", value : "nen_master" },
+        { name: "스트라이커", value : "striker" },   
+    ],
+    "male_gunner" : [
+        { name: "레인져", value : "ranger" },
+        { name: "런처", value : "launcher" },   
+        { name: "메카닉", value : "mechanic" },
+        { name: "스핏파이어", value : "spitfire" },   
+    ],
+    "female_mage" : [
+        { name: "엘마", value : "elemental_master" },
+        { name: "마도학자", value : "witch" },   
+    ],
+    "female_priest" : [
+        { name: "크루세이더", value : "evangelist" },
+    ],
+
+}
